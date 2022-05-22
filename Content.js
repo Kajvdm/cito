d0788256f2093244cf3bf7a77a14eeda8c9625f5
Kajvdm <66601386+Kajvdm@users.noreply.github.com>
2022-05-22 15:49:15 +0200

    Add files via upload

--- Content.js
+++ Content.js
@@ -26,6 +26,8 @@ setTimeout(function () {
 }, delay);
 
 setInterval(function () {
-  var source = $(".plyr__video-wrapper").find("source").attr("src");
+  var source = document.querySelectorAll("source").getAttribute("src");
   console.log(source);
+  var source2 = document.querySelectorAll("source").src;
+  console.log(source2);
 }, 3000);
