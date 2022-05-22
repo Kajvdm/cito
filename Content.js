e3943380160bfe79200f163a724435f9646d9e5a
Kajvdm <66601386+Kajvdm@users.noreply.github.com>
2022-05-22 15:40:47 +0200

    Add files via upload

---
+++ Content.js
@@ -0,0 +1,31 @@
+var script = document.createElement("script");
+script.src = "https://code.jquery.com/jquery-3.6.0.min.js";
+document.getElementsByTagName("head")[0].appendChild(script);
+
+var delay = 1000;
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+}
+
+setTimeout(function () {
+  closeFullscreen();
+  const dimmer = document.querySelector("#dimmer");
+  if (dimmer.classList.contains("visible")) {
+    dimmer.classList.remove("visible");
+  }
+  const qtiContent = document.querySelector("#qti-content");
+  if (qtiContent.classList.contains("disabled")) {
+    qtiContent.classList.remove("disabled");
+  }
+}, delay);
+
+setInterval(function () {
+  var source = $(".plyr__video-wrapper").find("source").attr("src");
+  console.log(source);
+}, 3000);
