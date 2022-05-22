9be363df18dda1c3125986cd419c247f069d187f
Kajvdm <66601386+Kajvdm@users.noreply.github.com>
2022-05-22 17:41:39 +0200

    Add files via upload

--- Content.js
+++ Content.js
@@ -2,24 +2,11 @@ let source = document.querySelector("source").getAttribute("src");
 console.log(source);
 
 var delay = 1000;
-function closeFullscreen() {
-  if (document.exitFullscreen) {
-    document.exitFullscreen();
-  } else if (document.webkitExitFullscreen) {
-    document.webkitExitFullscreen();
-  } else if (document.msExitFullscreen) {
-    document.msExitFullscreen();
-  }
+const dimmer = document.querySelector("#dimmer");
+if (dimmer.classList.contains("visible")) {
+  dimmer.classList.remove("visible");
 }
-
-setTimeout(function () {
-  closeFullscreen();
-  const dimmer = document.querySelector("#dimmer");
-  if (dimmer.classList.contains("visible")) {
-    dimmer.classList.remove("visible");
-  }
-  const qtiContent = document.querySelector("#qti-content");
-  if (qtiContent.classList.contains("disabled")) {
-    qtiContent.classList.remove("disabled");
-  }
-}, delay);
+const qtiContent = document.querySelector("#qti-content");
+if (qtiContent.classList.contains("disabled")) {
+  qtiContent.classList.remove("disabled");
+}
